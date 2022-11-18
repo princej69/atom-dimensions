2c053ec55bdb72241da5c4ce1c7da96b00eba3ab
i-cm <74533816+i-cm@users.noreply.github.com>
2022-11-18 22:18:35 +1100

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,77 @@
+const game = {
+    version: "pre-α 0.1"
+}
+function newPlayer() {
+    Player = {
+        money: ExpantaNum(10),
+        dimesion1: 0,
+        dimesion1cost: ExpantaNum(10),
+        dimesion1total: 0,
+        dimesion2: 0,
+        dimesion2cost: ExpantaNum(100),
+        dimesion2total: 0,
+        dimesion3: 0,
+        dimesion3cost: ExpantaNum(100),
+        dimesion3total: 0,
+        dimesion4: 0,
+        dimesion4cost: ExpantaNum(1000),
+        dimesion4total: 0,
+        dimesion5: 0,
+        dimesion5cost: ExpantaNum(1e6),
+        dimesion5total: 0,
+        dimesion6: 0,
+        dimesion6cost: ExpantaNum(1e9),
+        dimesion6total: 0,
+        dimesion7: 0,
+        dimesion7cost: ExpantaNum(1e12),
+        dimesion7total: 0,
+        dimesion8: 0,
+        dimesion8cost: ExpantaNum(1e15),
+        dimesion8total: 0,
+        dimboosts: 0,
+        dimgalaxy: 0,
+        infinity {
+            IP: 0,
+            Upgrades: [],
+            autobuyers: [],
+            Challenges: [],
+            BreakInfinity: {
+                unlocked: false,
+                breaked: false,
+                upgrades: []
+            },
+            dimensions: {
+                unlocked: false,
+                infinityPower: 10,
+                dimesion1: 0,
+                dimesion1cost: ExpantaNum(10),
+                dimesion1total: 0,
+                dimesion2: 0,
+                dimesion2cost: ExpantaNum(100),
+                dimesion2total: 0,
+                dimesion3: 0,
+                dimesion3cost: ExpantaNum(100),
+                dimesion3total: 0,
+                dimesion4: 0,
+                dimesion4cost: ExpantaNum(1000),
+                dimesion4total: 0,
+                dimesion5: 0,
+                dimesion5cost: ExpantaNum(1e6),
+                dimesion5total: 0,
+                dimesion6: 0,
+                dimesion6cost: ExpantaNum(1e9),
+                dimesion6total: 0,
+                dimesion7: 0,
+                dimesion7cost: ExpantaNum(1e12),
+                dimesion7total: 0,
+                dimesion8: 0,
+                dimesion8cost: ExpantaNum(1e15),
+                dimesion8total: 0,
+                dimboosts: 0,
+                dimgalaxy: 0
+            }
+        }
+    }
+}
+
+// Idk what's next
